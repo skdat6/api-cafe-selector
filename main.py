a209e35fb0578674cb6e0d8593f4c7e60404a055
skdat6 <dobretheo@gmail.com>
2022-09-18 15:25:35 +0300

    added POST, PATCH and DELETE functionalities

--- main.py
+++ main.py
@@ -94,11 +94,58 @@ def get_cafe_at_location():
         return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
 
 
-#TODO HTTP POST - Create
+def str_to_bool(arg_from_url):
+    if arg_from_url in ['True', ' true', 'T', 't', 'Yes', 'yes', 'y', '1']:
+        return True
+    else:
+        return False
 
-#TODO HTTP PUT/PATCH - Update
 
-#TODO HTTP DELETE - Delete
+@app.route("/add", methods=["GET", "POST"])
+def add_a_cafe():
+    new_cafe = Cafe(name=request.args.get("name"),
+                    map_url=request.args.get("map_url"),
+                    img_url=request.args.get("img_url"),
+                    location=request.args.get("location"),
+                    seats=request.args.get("seats"),
+                    has_toilet=str_to_bool(request.args.get("has_toilet")),
+                    has_wifi=str_to_bool(request.args.get("has_wifi")),
+                    has_sockets=str_to_bool(request.args.get("has_sockets")),
+                    can_take_calls=str_to_bool(request.args.get("can_take_calls")),
+                    coffee_price=request.args.get("coffee_price")
+                    )
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return jsonify(response={"success": "Successfully added the new cafe"})
+
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH", "GET", "POST"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the corresponding cafe price."})
+    else:
+        return jsonify(error={"Not Found": "Cafe id not found"})
+
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "testApiKey":
+        cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe"}), 200
+        else:
+            return jsonify(error={"Not Found": "Cafe id not found"}), 404
+    else:
+        return jsonify(error={"Forbidden": "Incorrect api key"}), 403
+
 
 
 if __name__ == '__main__':
